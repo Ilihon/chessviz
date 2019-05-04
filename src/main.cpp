d556dee08d57306bb8a3765e111659781b0fd7f7
Ilihon <Ilia.Burdukovskiy@gmail.com>
2019-05-04 18:01:50 +0700

    Some changes to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,23 @@
 #include "board.h"
-#include <iomanip>
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-using namespace std;
-
-int main()
-{
-    char pole[9][9] = {{'8', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-                       {'7', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-                       {'6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                       {'5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                       {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                       {'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                       {'2', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                       {'1', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-                       {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
-    ChessField(pole);
-
-    while (true) {
-        Move(pole);
-        ChessField(pole);
-    }
-    return 0;
+int main(){ 
+  char pole [9][9]={
+    {'8','r','n','b','q','k','b','n','r'},
+    {'7','p','p','p','p','p','p','p','p'},
+    {'6',' ',' ',' ',' ',' ',' ',' ',' '},
+    {'5',' ',' ',' ',' ',' ',' ',' ',' '},
+    {'4',' ',' ',' ',' ',' ',' ',' ',' '},
+    {'3',' ',' ',' ',' ',' ',' ',' ',' '},
+    {'2','P','P','P','P','P','P','P','P'},
+    {'1','R','N','B','Q','K','B','N','R'},
+    {' ','a','6','c','d','e','f','g','h'}
+  };
+  int team=0;
+  ChessField(pole);
+  while(true)
+  {
+  	Move(pole,&team);
+	ChessField(pole);
+  }
+  return 0;
 }
