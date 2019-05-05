b017786c74397a2384c45aa6a51d6096c2979ccb
Ilihon <Ilia.Burdukovskiy@gmail.com>
2019-05-05 16:27:37 +0700

    Small fixes

--- Makefile
+++ Makefile
@@ -1,16 +1,21 @@
 g = g++
 pars = -c -Wall -Werror
+
 file1 = src/main.cpp
 file2 = src/board.cpp
 files = $(file1) $(file2)
-binary = bin/chessviz
+
 object1 = build/src/main.o
 object2 = build/src/board.o
 objects = $(object1) $(object2)
 
+cbinary = bin/chessviz-test
+binary = bin/chessviz
+
+
 .PHONY: clean test
 
-all: $(binary)
+all: $(binary) clean
 
 $(object1): $(file1)
 	$(g) $(pars) $^ -o $@
@@ -22,9 +27,13 @@ $(binary): $(objects)
 	$(g) $^ -o $(binary)
 
 test: 
-	./bin/chessviz
+	$(binary)
+
+ctest:
+	$(cbinary)
+
 
 no-rm: $(binary)
 
 clean:
-	rm build/*.o
+	rm build/src/*.o
--- test/main.c
+++ test/main.c
@@ -69,20 +69,20 @@ CTEST (B_Peshka, Correct_p_attack)
 
 CTEST (W_Rook1, Correct_R1)
 {
-    int i1=6,j1=2,i2=4,j2=2;
-    pole[5][2]=' ';
+    int i1=7,j1=2,i2=5,j2=2;
+    pole[6][2]=' ';
     int result=MoveCheck(0, i1, i2, j1, j2, pole);
     int expected=1;
-    pole[5][2]='P';
+    pole[6][2]='P';
     ASSERT_EQUAL(expected, result);
 }
 CTEST (W_Rook2, Correct_R2)
 {
-    int i1=6,j1=8,i2=4,j2=8;
-    pole[5][8]=' ';
+    int i1=7,j1=8,i2=5,j2=8;
+    pole[6][8]=' ';
     int result=MoveCheck(0, i1, i2, j1, j2, pole);
     int expected=1;
-    pole[5][8]='P';
+    pole[6][8]='P';
     ASSERT_EQUAL(expected, result);
 }
 
@@ -104,6 +104,129 @@ CTEST (B_Rook2, Correct_r2)
     pole[1][8]='P';
     ASSERT_EQUAL(expected, result);
 }
+
+CTEST (W_Horse1, Correct_H1)
+{
+    int i1=7,j1=2,i2=5,j2=3;
+    int result=MoveCheck(0, i1, i2, j1, j2, pole);
+    int expected=1;
+    ASSERT_EQUAL(expected, result);
+}
+CTEST (W_Horse2, Correct_H2)
+{
+    int i1=7,j1=7,i2=5,j2=6;
+    int result=MoveCheck(0, i1, i2, j1, j2, pole);
+    int expected=1;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST (B_Horse1, Correct_h1)
+{
+    int i1=0,j1=2,i2=2,j2=3;
+    int result=MoveCheck(1, i1, i2, j1, j2, pole);
+    int expected=1;
+    ASSERT_EQUAL(expected, result);
+}
+CTEST (B_Horse2, Correct_h2)
+{
+    int i1=0,j1=7,i2=2,j2=3;
+    int result=MoveCheck(1, i1, i2, j1, j2, pole);
+    int expected=1;
+    ASSERT_EQUAL(expected, result);
+}
+
+
+CTEST (W_Bishop1, Correct_B1)
+{
+    pole[6][4]=' ';
+    int i1=7,j1=3,i2=5,j2=5;
+    int result=MoveCheck(0, i1, i2, j1, j2, pole);
+    int expected=1;
+    pole[6][4]='P';
+    ASSERT_EQUAL(expected, result);
+}
+CTEST (W_Bishop2, Correct_B2)
+{
+    pole[6][5]=' ';
+    int i1=7,j1=6,i2=5,j2=4;
+    int result=MoveCheck(0, i1, i2, j1, j2, pole);
+    int expected=1;
+    pole[6][5]='P';
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST (B_Bishop1, Correct_b1)
+{
+    pole[1][4]=' ';
+    int i1=0,j1=3,i2=2,j2=5;
+    int result=MoveCheck(1, i1, i2, j1, j2, pole);
+    int expected=1;
+    pole[1][4]='p';
+    ASSERT_EQUAL(expected, result);
+}
+CTEST (B_Bishop2, Correct_b2)
+{
+    pole[1][3]=' ';
+    int i1=0,j1=6,i2=2,j2=4;
+    int result=MoveCheck(1, i1, i2, j1, j2, pole);
+    int expected=1;
+    pole[1][3]='p';
+    ASSERT_EQUAL(expected, result);
+}
+
+
+CTEST (W_Queen, Correct_Q)
+{
+    pole[6][4]=' ';
+    int i1=7,j1=4,i2=5,j2=4;
+    int result=MoveCheck(0, i1, i2, j1, j2, pole);
+    int expected=1;
+    pole[6][4]='P';
+    ASSERT_EQUAL(expected, result);
+}
+CTEST (B_Queen, Correct_q)
+{
+    pole[1][4]=' ';
+    int i1=0,j1=4,i2=2;j2=4;
+    int result=MoveCheck(0, i1, i2, j1, j2, pole);
+    int expected=1;
+    pole[1][4]='p';
+    ASSERT_EQUAL(expected, result);
+}
+
+
+CTEST (W_King, Correct_K)
+{
+    pole[6][5]=' ';
+    int i1=7,j1=5,i2=6,j2=5;
+    int result=MoveCheck(0, i1, i2, j1, j2, pole);
+    int expected=1;
+    pole[6][5]='P';
+    ASSERT_EQUAL(expected, result);
+}
+CTEST (B_King, Correct_k)
+{
+    pole[1][5]=' ';
+    int i1=0,j1=5,i2=1,j2=5;
+    int result=MoveCheck(0, i1, i2, j1, j2, pole);
+    int expected=1;
+    pole[1][5]='p';
+    ASSERT_EQUAL(expected, result);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
